d437b61d9346ee8bd6c86874a14988b03a574092
Unkybr <x-renan.machado@hotmail.com>
2018-05-02 21:41:57 -0300

    Git inicial

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+	<title>Página Inicial</title>
+</head>
+
+<body>
+	<div class="container">
+		
+			<nav class="navbar navbar-expand-lg navbar-light row bg-dark">
+			  <a class="navbar-brand col-md-1" href="index.php"><img src="imagem/logo.png" width="100%"></a>
+			    <form class="form-inline col-md-8">
+			      <input class="form-control col-md-10 mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+			      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+			    </form>
+			   <div class="col-md-2"><a href="" >Login</a>
+			   	<span style="color: white;">ou</span>
+			   						<a href="" >Cadastre-se</a>
+			   </div>
+			</nav>
+
+			<nav class="navbar navbar-expand-lg navbar-dark bg-dark row">
+			  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+			    <span class="navbar-toggler-icon"></span>
+			  </button>
+			  <div class="col-md-2"></div>
+			  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+			    <div class="navbar-nav col-md-8 text-center">
+			      <a class="nav-item nav-link" href="#">Telefonia</a>
+			      <a class="nav-item nav-link" href="#">Informática</a>
+			      <a class="nav-item nav-link" href="#">Eletrônicos</a>
+			      <a class="nav-item nav-link" href="#">Eletrodomésticos</a>
+			      <a class="nav-item nav-link" href="#">Eletroportáteis</a>
+			      <a class="nav-item nav-link" href="#">Decoração</a>
+			      <a class="nav-item nav-link" href="#">Entretenimento</a>
+			    </div>
+			  </div>
+			</nav>
+			
+	</div>	
+	
+
+</body>
+</html>
